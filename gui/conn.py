ea2b6b8e6832e2e14d6decd5e5b815c1d4a91cbc
niallo <niallo@f3d975e6-390d-11de-b310-9106b1f7c239>
2007-12-11 07:29:44 +0000

    have the connection run in its own thread. this will be necessary for the "real" gui.
    git-svn-id: http://unworkable.googlecode.com/svn/trunk@433 f3d975e6-390d-11de-b310-9106b1f7c239

--- gui/conn.py
+++ gui/conn.py
@@ -1,6 +1,6 @@
 #!/usr/local/bin/python
 
-# $Id: conn.py,v 1.1 2007-12-10 03:57:01 niallo Exp $
+# $Id: conn.py,v 1.2 2007-12-11 07:29:44 niallo Exp $
 # Copyright (c) 2007 Niall O'Higgins <niallo@unworkable.org>
 #
 # Permission to use, copy, modify, and distribute this software for any
@@ -18,6 +18,8 @@
 import getopt
 import socket
 import sys
+import threading
+import time
 
 HOST = 'localhost'
 PORT = 6099
@@ -25,6 +27,75 @@ PORT = 6099
 def usage():
 	print >> sys.stderr, "%s: [-h host] [-p port]" %(sys.argv[0])
 	os._exit(1)
+
+def status(ctl):
+	print "peers: %s pieces: %s/%s downloaded: %s" %(len(ctl.peers), len(ctl.pieces), ctl.num_pieces, ctl.torrent_bytes + ctl.bytes)
+
+class CTLconnection(threading.Thread):
+	''' Connection to the control server, runs in its own thread '''
+	def __init__(self, host, port):
+		self.host = host
+		self.port = port
+		self.num_peers = 0
+		self.num_pieces = 0
+		self.torrent_size = 0
+		self.torrent_bytes = 0
+		self.pieces = []
+		self.peers = []
+		self.bytes = 0
+		self.done = False
+		self._socket = None
+		self._f = None
+		threading.Thread.__init__(self)
+	def stop(self):
+		self._f.close()
+		self._s.close()
+		self._done = True
+	def run(self):
+		self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		self._socket.connect((self.host, self.port))
+		self._f = self._socket.makefile()
+
+		for l in self._f:
+			try:
+				d = l.strip().split(':', 1)
+			except:
+				# ignore malformed line
+				continue
+			if d[0] == 'num_peers':
+				if not isinstance(d[1], int):
+					continue
+				self.num_peers = int(d[1])
+			elif d[0] == 'num_pieces':
+				self.num_pieces = int(d[1])
+			elif d[0] == 'torrent_size':
+				self.torrent_size = int(d[1])
+			elif d[0] == 'torrent_bytes':
+				self.torrent_bytes = int(d[1])
+			elif d[0] == 'pieces':
+				try:
+					new_pieces = d[1].split(',')
+					new_pieces.sort()
+					self.pieces = new_pieces
+				except:
+					# no pieces yet
+					continue
+			elif d[0] == 'bytes':
+				self.bytes = int(d[1])
+			elif d[0] == 'peers':
+				try:
+					new_peers = d[1].split(',')
+					new_peers.sort()
+					self.peers = new_peers
+				except:
+					# no peers yet
+					continue
+			else:
+				print "unkown message: %s" %(l)
+		self._f.close()
+		self._s.close()
+		self.done = True
+
 
 try:
 	opts, args = getopt.getopt(sys.argv[1:], "h:p:")
@@ -37,59 +108,8 @@ for o, a in opts:
 	if o == "-p":
 		PORT = int(a)
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((HOST, PORT))
-f = s.makefile()
-
-# initial state variables
-num_peers = 0
-num_pieces = 0
-torrent_size = 0
-torrent_bytes = 0
-pieces = []
-peers = []
-bytes = 0
-
-def status():
-	print "peers: %s pieces: %s/%s downloaded: %s" %(len(peers), len(pieces), num_pieces, torrent_bytes + bytes)
-
-for l in f:
-	try:
-		d = l.strip().split(':', 1)
-	except:
-		# ignore malformed line
-		continue
-	if d[0] == 'num_peers':
-		if not isinstance(d[1], int):
-			continue
-		num_peers = int(d[1])
-	elif d[0] == 'num_pieces':
-		num_pieces = int(d[1])
-	elif d[0] == 'torrent_size':
-		torrent_size = int(d[1])
-	elif d[0] == 'torrent_bytes':
-		torrent_bytes = int(d[1])
-	elif d[0] == 'pieces':
-		try:
-			new_pieces = d[1].split(',')
-			new_pieces.sort()
-			pieces = new_pieces
-		except:
-			# no pieces yet
-			continue
-	elif d[0] == 'bytes':
-		bytes = int(d[1])
-	elif d[0] == 'peers':
-		try:
-			new_peers = d[1].split(',')
-			new_peers.sort()
-			peers = new_peers
-		except:
-			# no peers yet
-			continue
-	else:
-		print "unkown message: %s" %(l)
-	status()
-
-f.close()
-s.close()
+ctl = CTLconnection(HOST, PORT)
+ctl.start()
+while not ctl.done:
+	status(ctl)
+	time.sleep(5)
